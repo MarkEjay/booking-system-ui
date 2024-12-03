f7b1d99089b9a2e78b071f4b1ea4f3959c349ca5
Mark Ejakpomewhe <markejay.am@gmail.com>
2024-12-03 16:55:05 -0500

    v2

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,6 +1,6 @@
 // export const BASEURL = 'https://booking-system-api-production.up.railway.app'
- export const BASEURL = 'http://localhost:5009'
-//  export const BASEURL = 'https://booking-system-api-03ps.onrender.com'
+//  export const BASEURL = 'http://localhost:5009'
+ export const BASEURL = 'https://booking-system-api-03ps.onrender.com'
 
 
 // export const BASEURL = '/api';
